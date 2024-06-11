8cce51552361742d37eda99aeea4823186224024
Ramón Martínez <ramartinez@aidacanarias.com>
2024-06-11 11:11:12 +0200

    implement ConsoleNotifier : Notifier

--- hello-service/HelloService/ConsoleNotifier.cs
+++ hello-service/HelloService/ConsoleNotifier.cs
@@ -2,12 +2,6 @@ using System;
 
 namespace Hello;
 
-public interface Notifier
-{
-    void Notify(string message);
-}
-
-
 public class ConsoleNotifier : Notifier
 {
     public void Notify(string message)
