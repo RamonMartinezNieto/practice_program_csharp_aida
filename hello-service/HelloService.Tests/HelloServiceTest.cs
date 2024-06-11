93e850df014f44737ef379a3638318305747eb73
Ramón Martínez <ramartinez@aidacanarias.com>
2024-06-11 10:20:41 +0200

    refactor create builder for DateTime

--- hello-service/HelloService.Tests/HelloServiceTest.cs
+++ hello-service/HelloService.Tests/HelloServiceTest.cs
@@ -6,17 +6,63 @@ namespace Hello.Tests;
 
 public class HelloServiceTest
 {
+    private Notifier _notifier;
+    private DateProvider _dateProvider;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _notifier = Substitute.For<Notifier>();
+        _dateProvider = Substitute.For<DateProvider>();
+    }
+
     [Test]
     public void Say_BuenosDias_At_8AM()
     {
-        Notifier notifier = Substitute.For<Notifier>();
-        DateProvider dateProvider = Substitute.For<DateProvider>();
-        dateProvider.GetDate().Returns(new DateTime(2024, 06, 11, 08, 00, 00, DateTimeKind.Utc));
+        _dateProvider.GetDate().Returns(DateTimeBuilder.DefaultDateTime().WithHour(8).Build());
 
-        HelloService helloService = new(notifier, dateProvider);
+        HelloService helloService = new(_notifier, _dateProvider);
 
         helloService.Hello();
 
-        notifier.Received(1).SayGoodMorning();
+        _notifier.Received(1).SayGoodMorning();
+    }
+}
+
+public class DateTimeBuilder
+{
+    private int _year;
+    private int _month;
+    private int _day;
+    private int _hour;
+    private int _minute;
+    private int _second;
+    private DateTimeKind _kind;
+
+    private DateTimeBuilder()
+    {
+        _year = 2024;
+        _month = 6;
+        _day = 11;
+        _hour = 0;
+        _minute = 0;
+        _second = 0;
+        _kind = DateTimeKind.Utc;
+    }
+
+    public DateTimeBuilder WithHour(int hour)
+    {
+        _hour = hour;
+        return this;
+    }
+
+    public static DateTimeBuilder DefaultDateTime()
+    {
+        return new DateTimeBuilder();
+    }
+
+    public DateTime Build()
+    {
+        return new DateTime(_year, _month, _day, _hour, _minute, _second, _kind);
     }
 }
