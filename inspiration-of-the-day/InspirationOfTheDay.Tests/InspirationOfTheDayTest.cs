6c4ca6f5395feadfbd1e85902ead2a9cfd3871d1
Ramón Martínez <ramartinez@aidacanarias.com>
2024-06-28 10:27:41 +0200

    test SendInspirationToEmployee

--- inspiration-of-the-day/InspirationOfTheDay.Tests/InspirationOfTheDayTest.cs
+++ inspiration-of-the-day/InspirationOfTheDay.Tests/InspirationOfTheDayTest.cs
@@ -1,13 +1,67 @@
+using NSubstitute;
 using NUnit.Framework;
 
-namespace InspirationOfTheDay.Tests
+namespace InspirationOfTheDay.Tests;
+
+public class InspirationOfTheDayTest
 {
-    public class InspirationOfTheDayTest
+    [Test]
+    public void SendInspirationToEmployee()
     {
-        [Test]
-        public void GetListOfQuotes()
-        {
-            Assert.That(false, Is.True);
-        }
+        QuotesService quotesService = Substitute.For<QuotesService>();
+        Sender sender = Substitute.For<Sender>();
+        RandomWrapper random = Substitute.For<RandomWrapper>();
+        InspireService inspireService = new(quotesService, sender, random);
+
+        inspireService.InspireSomeone("pato");
+
+        sender.Received(1).SendInspiration("pato uno", new Employee("Ramon"));
+    }
+}
+
+public interface QuotesService
+{
+}
+
+public interface Sender
+{
+    void SendInspiration(string quote, Employee employee);
+}
+
+public interface RandomWrapper
+{
+}
+
+public class InspireService
+{
+    private readonly QuotesService _quotesService;
+    private readonly Sender _sender;
+    private readonly RandomWrapper _random;
+
+    public InspireService(QuotesService quotesService, Sender sender, RandomWrapper random)
+    {
+        _quotesService = quotesService;
+        _sender = sender;
+        _random = random;
+    }
+
+    public void InspireSomeone(string word)
+    {
+        _sender.SendInspiration("pato uno", new Employee("Ramon"));
+    }
+}
+
+public record class Employee(string Name)
+{
+    public string Name { get; init; }
+}
+
+public class Employees
+{
+    private readonly RandomWrapper _random;
+
+    public Employees(RandomWrapper random)
+    {
+        _random = random;
     }
 }
