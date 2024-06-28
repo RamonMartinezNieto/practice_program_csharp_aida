4b286d77395e9a838565454580d74ab7c5e01a2f
Ramón Martínez <ramartinez@aidacanarias.com>
2024-06-28 11:51:42 +0200

    refactor suite of tests

--- inspiration-of-the-day/InspirationOfTheDay.Tests/InspirationOfTheDayTest.cs
+++ inspiration-of-the-day/InspirationOfTheDay.Tests/InspirationOfTheDayTest.cs
@@ -26,34 +26,43 @@ public class InspirationOfTheDayTest
     public void SendInspirationWithOnlyOneQuoteAndAnEmployee()
     {
         _employees.GetAll().Returns(new List<Employee>() { new("999") });
-        _quotesService.GetListOfQuotesWith("pato").Returns(new List<Quote>() { new("pato uno") });
+        _quotesService.GetListOfQuotesWith("pato").Returns(new List<Quote>() { new($"pato uno") });
 
         _inspireService.InspireSomeone("pato");
-
-        _quoteSender.Received(1).Send(new Quote("pato uno"), new ContactData("999"));
+        ThenReceivedSend("pato uno", "999");
     }
 
     [Test]
     public void WithSeveralEmployeesAndOnlyOneQuote()
     {
-        _employees.GetAll().Returns(new List<Employee>() { new("999"), new("888") });
-        _random.GetRandomNumberOf(2).Returns(1);
+        GivenListOfEmployees();
         _quotesService.GetListOfQuotesWith("superman").Returns(new List<Quote>() { new("superman uno") });
+        _random.GetRandomNumberOf(2).Returns(1);
 
         _inspireService.InspireSomeone("superman");
 
-        _quoteSender.Received(1).Send(new Quote("superman uno"), new ContactData("888"));
+        ThenReceivedSend("superman uno", "888");
     }
 
     [Test]
     public void WithSeveralEmployeesAndSeveralQuotes()
     {
-        _employees.GetAll().Returns(new List<Employee>() { new("999"), new("888") });
+        GivenListOfEmployees();
         _quotesService.GetListOfQuotesWith("superman").Returns(new List<Quote>() { new("superman uno"), new("superman dos") });
         _random.GetRandomNumberOf(Arg.Any<int>()).Returns(1, 1);
 
         _inspireService.InspireSomeone("superman");
 
-        _quoteSender.Received(1).Send(new Quote("superman dos"), new ContactData("888"));
+        ThenReceivedSend("superman dos", "888");
+    }
+
+    private void ThenReceivedSend(string quote, string telephone)
+    {
+        _quoteSender.Received(1).Send(new Quote(quote), new ContactData(telephone));
+    }
+
+    private void GivenListOfEmployees()
+    {
+        _employees.GetAll().Returns(new List<Employee>() { new("999"), new("888") });
     }
 }
--- inspiration-of-the-day/InspirationOfTheDay/InspireService.cs
+++ inspiration-of-the-day/InspirationOfTheDay/InspireService.cs
@@ -7,7 +7,11 @@ public class InspireService
     private readonly RandomNumberGenerator _random;
     private readonly Employees _employees;
 
-    public InspireService(QuotesService quotesService, QuoteSender quoteSender, RandomNumberGenerator random, Employees employees)
+    public InspireService(
+        QuotesService quotesService,
+        QuoteSender quoteSender,
+        RandomNumberGenerator random,
+        Employees employees)
     {
         _quotesService = quotesService;
         _quoteSender = quoteSender;
@@ -17,12 +21,20 @@ public class InspireService
 
     public void InspireSomeone(string word)
     {
+        _quoteSender.Send(GetQuote(word), GetContactData());
+    }
+
+    private ContactData GetContactData()
+    {
         var employees = _employees.GetAll();
         var indexEmployee = _random.GetRandomNumberOf(employees.Count);
+        return employees[indexEmployee].GetContactData();
+    }
 
+    private Quote GetQuote(string word)
+    {
         var quotes = _quotesService.GetListOfQuotesWith(word);
         var indexQuote = _random.GetRandomNumberOf(quotes.Count);
-
-        _quoteSender.Send(quotes[indexQuote], employees[indexEmployee].GetContactData());
+        return quotes[indexQuote];
     }
 }
